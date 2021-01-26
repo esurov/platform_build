08ccdddd78e5a4aac36b21b77943a94224977ba6
Aurelien BOUIN <aurelien.bouin@captina.dev>
2021-01-26 18:53:41 +0100

    Add for emulator prebuilts APKs : Vpn Tchat FDroid SimpleFileManager

--- target/product/aosp_x86_64.mk
+++ target/product/aosp_x86_64.mk
@@ -64,6 +64,7 @@ ifeq (aosp_x86_64,$(TARGET_PRODUCT))
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_release.mk)
 endif
 
+PRODUCT_PACKAGES += Vpn Tchat FDroid SimpleFileManager
 
 PRODUCT_NAME := aosp_x86_64
 PRODUCT_DEVICE := generic_x86_64
